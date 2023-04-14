76d0ce1412765c3e030ddad9e4400927d6c8ba3e
Jordan Hancock <jhancock@chicagobooth.edu>
2023-04-14 04:49:26 +0000

    Exchange rate completed

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -13,10 +13,21 @@ class CurrenciesController < ApplicationController
     @parsed_data = JSON.parse(@raw_data)
     @symbols_hash = @parsed_data.fetch("symbols")
     @symbols = @symbols_hash.keys
-    
+
     @first_currency = params.fetch("first_currency")
 
     render({ :template => "currency_templates/step_two.html.erb"})
+  end
+  
+  def exchange_rate
+    @first_currency = params.fetch("first_currency")
+    @second_currency = params.fetch("second_currency")
+    xr_link = "https://api.exchangerate.host/convert?from=" + @first_currency + "&to=" + @second_currency
+    @raw_data = open(xr_link).read
+    @parsed_data = JSON.parse(@raw_data)
+
+    @xr = @parsed_data.fetch("info").fetch("rate")
+    render({  :template => "currency_templates/step_three.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get("/forex", { :controller => "currencies", :action => "first_currency"})
   get("/forex/:first_currency", { :controller => "currencies", :action => "second_currency"})
-  
+  get("/forex/:first_currency/:second_currency", { :controller => "currencies", :action => "exchange_rate"})
+
 end
