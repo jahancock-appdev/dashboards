0db12354887f71b4814037275a54e5c94b0d1c75
Jordan Hancock <jhancock@chicagobooth.edu>
2023-04-14 04:36:41 +0000

    Second currency completed

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -8,4 +8,15 @@ class CurrenciesController < ApplicationController
     render({ :template => "currency_templates/step_one.html.erb"})
   end
 
+  def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @symbols = @symbols_hash.keys
+    
+    @first_currency = params.fetch("first_currency")
+
+    render({ :template => "currency_templates/step_two.html.erb"})
+  end
+
 end
